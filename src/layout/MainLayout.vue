2af28ce231bb6e6500e84b0e5aa499efca0db90b
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-04-25 13:12:40 +0300

    bank

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div class="container with-nav">
+    <div class="card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
